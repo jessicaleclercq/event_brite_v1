b71c8f2f75b15a0e60a923b5c2a52c4cf47c4306
jessica leclercq <jessica_leclercq@hotmail.fr>
2020-05-11 20:43:19 +0200

    deuxième essai

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'jessica_leclercq@hotmail.fr'
   layout 'mailer'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
   has_many :events, foreign_key: 'admin_id', class_name: 'Event'
   has_many :attendances
   has_many :events, through: :attendances
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+  
 end
