41c6c735b07550effae26cd65d10e0827e6ed05e
jessica leclercq <jessica_leclercq@hotmail.fr>
2020-05-12 23:57:05 +0200

    pour heroku

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,7 +1,16 @@
 class EventController < ApplicationController
   
+  # View a list of events
   def index
      @events = Event.all
+  end
+
+  def show
+      @event = Event.find(params[:id])
+  end  
+
+  def new
+    @event = Event.new
   end
 
 end
