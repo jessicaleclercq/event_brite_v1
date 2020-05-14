c819903a41c49c858664a8ab4367484f9bf812ff
jessica leclercq <jessica_leclercq@hotmail.fr>
2020-05-14 13:54:46 +0200

    site fonctionnel basic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:show, :edit]
+
   # View a list of events
   def index
      @events = Event.all
@@ -7,7 +8,7 @@ class EventsController < ApplicationController
 
   def show
       @event = Event.find(params[:id])
-      if @event.admin_id == current_user.id
+      if @event.admin_id == current_user
         @admin = current_user
       end  
   end
@@ -24,7 +25,27 @@ class EventsController < ApplicationController
       flash[:danger] = "You must fill all the fields !"
       redirect_to new_event_path
     end
-
   end  
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+def update
+    @event = Event.find(params[:id])
+    if @event.update(title: params[:title], duration: params[:duration], description: params[:description], price: params[:price],start_date: params[:start_date], location: params[:location], admin_id: current_user.id)
+      redirect_to root_path
+      flash[:success] = "The event was modified"
+    else
+      render :edit
+      flash[:error] = "Please retry your modification"
+    end
 end
+
+  def destroy
+    Event.find(params[:id]).delete
+    redirect_to root_path
+  end
+
+end
+
